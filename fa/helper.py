038ac796f5e0df38da6ec727f08b415390a43372
Rendyindo <rendyarya22@gmail.com>
2018-05-29 11:20:24 +0700

    Added rate limit Please don't spank FA's server :(

--- fa/helper.py
+++ fa/helper.py
@@ -1,5 +1,21 @@
 import requests
 
+def RateLimit(maxPerSecond):
+    minInterval = 1.0 / float(maxPerSecond)
+    def decorate(func):
+        lastTimeCalled = [0.0]
+        def rateLimitedFunction(*args,**kargs):
+            elapsed = time.clock() - lastTimeCalled[0]
+            leftToWait = minInterval - elapsed
+            if leftToWait>0:
+                time.sleep(leftToWait)
+            ret = func(*args,**kargs)
+            lastTimeCalled[0] = time.clock()
+            return ret
+        return rateLimitedFunction
+    return decorate
+
+@RateLimit(2)
 def getpost(id, logincookie):
     r = requests.get("http://www.furaffinity.net/view/" + id, cookies=logincookie)
     if r.status_code == 200:
@@ -7,6 +23,7 @@ def getpost(id, logincookie):
     else:
         return r.raise_for_status()
 
+@RateLimit(2)
 def getuser(id, logincookie):
     r = requests.get("http://www.furaffinity.net/user/" + id, cookies=logincookie)
     if r.status_code == 200:
@@ -14,6 +31,7 @@ def getuser(id, logincookie):
     else:
         return r.raise_for_status()
 
+@RateLimit(2)
 def getjournal(id, logincookie):
     r = requests.get("http://www.furaffinity.net/journal/" + id, cookies=logincookie)
     if r.status_code == 200:
