99247854c8b8669278097584b292201ae0d7d069
Rendy Arya Kemal <rendyarya22@gmail.com>
2018-07-21 17:20:37 +0700

    int -> str

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/Rendyindo/FurAffinity-Python/'
 EMAIL = 'rendyarya22@gmail.com'
 AUTHOR = 'Rendy Aya Kemal'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = 0.0.1
+VERSION = '0.0.1'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
