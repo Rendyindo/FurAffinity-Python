4f05a602d2ced74723d63449b65a0bc846b6c839
Rendy Arya Kemal <rendyarya22@gmail.com>
2019-05-18 10:56:08 +0700

    Use perf_counter instead of process_time

--- fa/helper.py
+++ fa/helper.py
@@ -6,7 +6,7 @@ def RateLimit(maxPerSecond):
     def decorate(func):
         lastTimeCalled = [0.0]
         def rateLimitedFunction(*args,**kargs):
-            elapsed = time.process_time() - lastTimeCalled[0]
+            elapsed = time.perf_counter() - lastTimeCalled[0]
             leftToWait = minInterval - elapsed
             if leftToWait>0:
                 time.sleep(leftToWait)
