28408d00c7bd07893736436af5d9f3721acca31d
Rendy Arya Kemal <rendyarya22@gmail.com>
2019-05-18 12:56:24 +0700

    Add ability to login using username
    A Tkinter window will open, asking to answer captcha in order to login

--- fa/__init__.py
+++ fa/__init__.py
@@ -1,3 +1,4 @@
 from .fa import FurAffinity
+from .account import login_password
 from .__version__ import __title__, __description__, __url__, __version__
 from .__version__ import __author__, __author_email__
--- fa/account.py
+++ fa/account.py
@@ -1,6 +1,9 @@
 from fa.exceptions import Forbidden, NotLoggedIn
 from bs4 import BeautifulSoup
 import requests
+import time
+from tkinter import *
+import os
 
 logincookie = None
 username = None
@@ -21,3 +24,57 @@ def login(a="", b="", cfuid=""):
         raise NotLoggedIn("Invalid cookie.")
     logincookie = temp_cookie
 
+def login_password(username, password):
+    from PIL import Image, ImageTk
+    
+    s = requests.session()
+
+    def download_file(url):
+        local_filename = "captcha.jpg"
+        with s.get(url, stream=True) as r:
+            r.raise_for_status()
+            with open(local_filename, 'wb') as f:
+                for chunk in r.iter_content(chunk_size=8192): 
+                    if chunk:
+                        f.write(chunk)
+
+    cookie = {
+        "action":"login",
+        "name":username,
+        "pass":password,
+        "g-recaptcha-response":"",
+        "use_old_captcha":1,
+        "captcha":None,
+        "login":"Login+to%C2%A0FurAffinity"
+    }
+    s.get("https://www.furaffinity.net/login/") # Just to get a and __cfduid cookie :3c
+    download_file("https://www.furaffinity.net/captcha.jpg?random=" + str(int(time.time())))
+
+    class CaptchaFrame(Frame):
+        def __init__(self, root):
+            self.root = root
+            super().__init__(root)
+
+            self.image = ImageTk.PhotoImage(Image.open("captcha.jpg"))
+            self.imgpanel = Label(root, image=self.image).grid(row=0)
+            Label(root, text="Captcha").grid(row=1)
+            self.entry = Entry(root)
+            self.entry.grid(row=1, column=1)
+            #self.contents = StringVar()
+            #self.entry["textvariable"] = self.contents
+            Button(root, text='Submit', command=self.post).grid(row=3, column=0, sticky=W, pady=4)
+
+        def post(self):
+            cookie['captcha'] = self.entry.get()
+            ra = s.post("https://www.furaffinity.net/login/", data=cookie)
+            self.root.destroy()
+            os.remove("captcha.jpg")
+            if "erroneous" in ra.text:
+                raise Exception("Wrong captcha or password")
+
+    root = Tk()
+    lf = CaptchaFrame(root)
+    root.title("Captcha")
+    root.mainloop()
+    cookies = requests.utils.dict_from_cookiejar(s.cookies)
+    return cookies if len(cookies) != 2 else None
