68c7fcd7bcca7b704b1964325362987fcbb3c921
Rendy Arya Kemal <rendyarya22@gmail.com>
2019-05-18 10:42:59 +0700

    Remove selenium and add package infos

--- fa/__init__.py
+++ fa/__init__.py
@@ -1,1 +1,3 @@
-from .fa import *
+from .fa import FurAffinity
+from .__version__ import __title__, __description__, __url__, __version__
+from .__version__ import __author__, __author_email__
---
+++ fa/__version__.py
@@ -0,0 +1,6 @@
+__title__ = 'pyfa'
+__description__ = 'FurAffinity wrapper for python.'
+__url__ = 'https://github.com/rorre/FurAffinity-Python'
+__version__ = '0.0.1'
+__author__ = 'Rendy Arya Kemal'
+__author_email__ = 'rendyarya22@gmail.com'
